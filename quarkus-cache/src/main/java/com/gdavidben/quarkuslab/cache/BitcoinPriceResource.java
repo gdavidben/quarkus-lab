e5f17f90d4acd00b6b1eb99ffce7226debd23dbe
ESL Finder <contact.eslfinder@gmail.com>
2024-05-20 15:49:58 +0100

    feat: update project
    Signed-off-by: ESL Finder <contact.eslfinder@gmail.com>

--- quarkus-cache/src/main/java/com/gdavidben/quarkuslab/cache/BitcoinPriceResource.java
+++ quarkus-cache/src/main/java/com/gdavidben/quarkuslab/cache/BitcoinPriceResource.java
@@ -2,25 +2,25 @@ package com.gdavidben.quarkuslab.cache;
 
 import io.quarkus.cache.CacheInvalidateAll;
 import io.quarkus.cache.CacheResult;
+import jakarta.ws.rs.core.MediaType;
 import org.eclipse.microprofile.rest.client.inject.RestClient;
 
-import javax.enterprise.context.ApplicationScoped;
-import javax.inject.Inject;
-import javax.ws.rs.Consumes;
-import javax.ws.rs.GET;
-import javax.ws.rs.InternalServerErrorException;
-import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
-import javax.ws.rs.Produces;
-import javax.ws.rs.core.Response;
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
+import jakarta.ws.rs.Consumes;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.InternalServerErrorException;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.PathParam;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.Response;
 
 import static java.util.Optional.ofNullable;
-import static javax.ws.rs.core.MediaType.APPLICATION_JSON;
 
 @Path("/bitcoin/price")
 @ApplicationScoped
-@Consumes(APPLICATION_JSON)
-@Produces(APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
+@Produces(MediaType.APPLICATION_JSON)
 public class BitcoinPriceResource {
 
   @Inject
